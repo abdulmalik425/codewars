de01b8ec29fc5dbdec0c5db46200f74b580b0ece
Abdulmalik425 <malik.salah98@gmail.com>
2022-08-06 18:41:23 -0500

    awesome one

---
+++ 10areaAndVolume/areaAndVolume.js
@@ -0,0 +1,7 @@
+//Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+
+const getSize = (w, h, d) => [
+    (w * h + w * d + h * d) * 2,
+    w * h * d
+  ];
