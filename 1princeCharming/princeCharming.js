7a5c1ca770d8418151895d175ae6241336ef7ebe
Abdulmalik425 <malik.salah98@gmail.com>
2022-08-07 19:02:58 -0500

    First Try

---
+++ 1princeCharming/princeCharming.js
@@ -0,0 +1,8 @@
+// Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+//Return True if yes, False otherwise :)
+
+
+function hero(bullets, dragons){
+    return bullets >= dragons * 2
+  }
